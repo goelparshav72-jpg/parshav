567c010dabda52c9175777e5df0ef3093d661407
goelparshav72-jpg <goelparshav72@gmail.com>
2026-08-26 20:34:04 +0530

    Add files via upload

---
+++ day17 q33.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int num, originalNum, remainder, result = 0, n = 0;
+
+    
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    originalNum = num;
+
+    
+    while (originalNum != 0) {
+        originalNum /= 10;
+        n++;
+    }
+
+    originalNum = num;
+
+    
+    while (originalNum != 0) {
+        remainder = originalNum % 10;
+        
+        
+        result += round(pow(remainder, n)); 
+        
+        originalNum /= 10;
+    }
+
+    
+    if (result == num) {
+        printf("Armstrong\n");
+    } else {
+        printf("Not Armstrong\n");
+    }
+
+    return 0;
+}
