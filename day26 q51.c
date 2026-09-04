47aed2afb727389883f94a74c2f5c64f6b88c679
goelparshav72-jpg <goelparshav72@gmail.com>
2026-09-04 21:33:57 +0530

    Add files via upload

---
+++ day26 q51.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, k;
+
+    for(i = 1; i <= 5; i++)
+    {
+        
+        for(j = 1; j <= 5 - i; j++)
+        {
+            printf(" ");
+        }
+
+        
+        for(k = 6 - i; k <= 5; k++)
+        {
+            printf("%d", k);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
