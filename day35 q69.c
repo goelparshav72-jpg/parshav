c2334ff629dcbe18e5bde535dd4b0dfea3147378
goelparshav72-jpg <goelparshav72@gmail.com>
2026-09-13 19:05:29 +0530

    Add files via upload

---
+++ day35 q69.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int largest = INT_MIN;
+    int secondLargest = INT_MIN;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        } else if (arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+        }
+    }
+
+    printf("%d\n", secondLargest);
+
+    return 0;
+}
