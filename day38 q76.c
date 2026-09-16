5c365cb96f4b454d0503c4dab8e23604a6ce1c41
goelparshav72-jpg <goelparshav72@gmail.com>
2026-09-16 19:56:46 +0530

    Add files via upload

---
+++ day38 q76.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int main()
+{
+    int r, c;
+    int a[10][10];
+    int i, j;
+    int symmetric = 1;
+
+    // Input rows and columns
+    scanf("%d %d", &r, &c);
+
+    // Input matrix
+    for(i = 0; i < r; i++)
+    {
+        for(j = 0; j < c; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Check if matrix is square
+    if(r != c)
+    {
+        printf("False");
+        return 0;
+    }
+
+    // Check symmetry
+    for(i = 0; i < r; i++)
+    {
+        for(j = 0; j < c; j++)
+        {
+            if(a[i][j] != a[j][i])
+            {
+                symmetric = 0;
+                break;
+            }
+        }
+
+        if(symmetric == 0)
+        {
+            break;
+        }
+    }
+
+    if(symmetric == 1)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+
+    return 0;
+}
