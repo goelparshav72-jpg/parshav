873cb1ed188b45454e953d726ac19b2b4c2ff1fb
goelparshav72-jpg <goelparshav72@gmail.com>
2026-09-20 17:31:50 +0530

    Add files via upload

---
+++ day42 q83.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int vowels = 0, consonants = 0;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", str);
+
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if ((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z'))
+        {
+            if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' ||
+                str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+            {
+                vowels++;
+            }
+            else
+            {
+                consonants++;
+            }
+        }
+    }
+
+    printf("Vowels=%d, Consonants=%d", vowels, consonants);
+
+    return 0;
+}
