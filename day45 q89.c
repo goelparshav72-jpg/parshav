535b778c297aceac91f1c93d20b044ccb146cb87
goelparshav72-jpg <goelparshav72@gmail.com>
2026-09-23 21:14:40 +0530

    Add files via upload

---
+++ day45 q89.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100], ch;
+    int count = 0;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", str);
+
+    printf("Enter the character: ");
+    scanf(" %c", &ch);
+
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] == ch)
+        {
+            count++;
+        }
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
