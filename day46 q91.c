c0e202c2437c7dd02a936518deac24aa8bc5918a
goelparshav72-jpg <goelparshav72@gmail.com>
2026-09-24 16:37:23 +0530

    Add files via upload

---
+++ day46 q91.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", str);
+
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] != 'a' && str[i] != 'e' && str[i] != 'i' &&
+            str[i] != 'o' && str[i] != 'u' &&
+            str[i] != 'A' && str[i] != 'E' && str[i] != 'I' &&
+            str[i] != 'O' && str[i] != 'U')
+        {
+            printf("%c", str[i]);
+        }
+    }
+
+    return 0;
+}
